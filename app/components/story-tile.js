abd46615486a9e5bf130e8a60a0e1845243e21dc
Guest User <student@epicodus.com>
2015-09-22 15:15:26 -0700

    using ember-carousel for our carousels with some bootstrap styling

--- app/components/story-tile.js
+++ app/components/story-tile.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    prevStory: function(story) {
+      $('#story_' + story.id).toggleClass('carousel-hidden')
+    },
+    nextStory: function(story) {
+      $('#story_' + story.id).toggleClass('carousel-hidden')
+    }
+  }
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -2,8 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    var stories = this.store.findAll('story')
-                    .catch(e => {console.log(e.errors);});
-    return stories;
+    return this.store.findAll('story')
   },
 });
